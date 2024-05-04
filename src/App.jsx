a54e40fddd8dd9970986b474d5bc9a9bdd36e034
Vladyslav <dr.tienam123@gmail.com>
2024-05-04 04:44:11 +0300

    Initialize repozitory and ready to the develop

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+    return (
+        <>
+            <h1 className='all-links'
+                key='1234'
+                value={'asd'}
+            >
+                Hello
+            </h1>
+        </>
+    );
+};
+export default App;
